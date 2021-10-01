49f4dd31428d363e111e3b27081f6169ee6d7756
Atharva Kalsekar <atharva.kalsekar@whatfix.com>
2021-10-01 17:41:21 +0530

    inital commit

---
+++ src/app/layout/components/self-help/index.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+export default class SelfHelp extends React.Component {
+
+    constructor(props:any){
+        super(props);
+    }
+
+    render() {
+
+        return (
+            <div>
+                Self-help
+            </div>
+        );
+    }
+}
--- src/app/routes/Router.tsx
+++ src/app/routes/Router.tsx
@@ -5,16 +5,17 @@ import Planner from "layout/components/planner";
 
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Routes from "./Routes.json";
+import SelfHelp from "layout/components/self-help";
 
 export default function Router() {
   return (
     <AppContainer>
       <BrowserRouter>
         <Switch>
-          <Route path={Routes.LANDING_ROUTE} component={Planner} exact />
-          <Route path={Routes.LANDING_ROUTE} component={()=><React.Fragment></React.Fragment>} exact />
-          <Route path={Routes.LANDING_ROUTE} component={()=><React.Fragment></React.Fragment>} exact />
-          <Route path={Routes.LANDING_ROUTE} component={()=><React.Fragment></React.Fragment>} exact />
+          <Route path={Routes.LANDING_ROUTE} component={SelfHelp} exact />
+          <Route path={Routes.SELF_HELP} component={SelfHelp} exact />
+          {/* <Route path={Routes.LANDING_ROUTE} component={()=><React.Fragment></React.Fragment>} exact />
+          <Route path={Routes.LANDING_ROUTE} component={()=><React.Fragment></React.Fragment>} exact /> */}
         </Switch>
       </BrowserRouter>
     </AppContainer>
